de91dc711fe5ad14deeacd6ec92f5695d4d1510d
abbas <abbasmomeny1994@gmail.com>
2024-05-29 22:29:09 +0330

    ref: smsFactory

--- app/Modules/Sms/SmsFactory.php
+++ app/Modules/Sms/SmsFactory.php
@@ -4,28 +4,19 @@ namespace App\Modules\Sms;
 
 use App\Models\CreditCard;
 use App\Modules\Sms\Enums\Action;
-use App\Modules\Sms\Enums\Provider;
 use App\Modules\Sms\Providers\Ghasedak;
 use App\Modules\Sms\Providers\Kavenegar;
-use Illuminate\Http\Client\ConnectionException;
 
 class SmsFactory
 {
 
-    public function __construct(private readonly Provider $provider )
+    public static function send(CreditCard $creditCard, Action $action, array $data): void
     {
-    }
-
-    /**
-     * @throws ConnectionException
-     */
-    public function Send(CreditCard $creditCard, Action $action, array $data): void
-    {
-        switch ($this->provider->value) {
-            case Provider::KAVENEGAR->value:
+        switch (env('SMS_PROVIDER')) {
+            case 'kavenegar':
                 (new Kavenegar())->send($creditCard, $action, $data);
                 break;
-            case Provider::GASEDAK->value:
+            case 'ghasedak':
                 (new Ghasedak())->send($creditCard, $action, $data);
                 break;
         }
