68e35b8585370db6882b84765178cba6b4b65553
abbas <abbasmomeny1994@gmail.com>
2024-05-29 23:50:06 +0330

    add: card validation rule

--- app/Http/Requests/TransferRequest.php
+++ app/Http/Requests/TransferRequest.php
@@ -9,6 +9,7 @@ class TransferRequest extends FormRequest
 {
     protected function prepareForValidation()
     {
+        //change to middleware
         $this->merge([
             'source_card_number'      => (int)convertPersianToEnglish($this->request->get('source_card_number')),
             'destination_card_number' => (int)convertPersianToEnglish($this->request->get('destination_card_number')),
@@ -18,17 +19,12 @@ class TransferRequest extends FormRequest
 
     }
 
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     public function rules(): array
     {
         return [
-            'source_card_number'      => ['required', 'numeric', 'digits:16', 'exists:credit_cards,number'],
-            'destination_card_number' => ['required', 'numeric', 'digits:16', 'exists:credit_cards,number'],
-            'amount'                  => ['required', 'numeric', 'min:1000', 'max:50000000'],
+            'source_card_number'      => ['required', 'integer', new ValidIranCreditCardNumber(), 'digits:16', 'exists:credit_cards,number'],
+            'destination_card_number' => ['required', 'integer', 'digits:16', 'exists:credit_cards,number'],
+            'amount'                  => ['required', 'integer', 'min:1000', 'max:50000000'],
         ];
     }
 }
