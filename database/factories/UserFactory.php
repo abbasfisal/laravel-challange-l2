6a7932cb5165242c8288e9a7250fec2cf2a77364
abbas <abbasmomeny1994@gmail.com>
2024-05-29 23:35:21 +0330

    ref: remove comments

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,20 +25,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name'   => fake()->name(),
-            'mobile' => "0912000" . rand(1111, 9999),
+            'name'           => fake()->name(),
+            'mobile'         => "0912000" . rand(1111, 9999),
             'password'       => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
-
-//    /**
-//     * Indicate that the model's email address should be unverified.
-//     */
-//    public function unverified(): static
-//    {
-//        return $this->state(fn (array $attributes) => [
-//            'email_verified_at' => null,
-//        ]);
-//    }
 }
