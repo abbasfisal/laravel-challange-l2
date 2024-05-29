474899bddd1139487896add4f0d7394a47fa0f45
abbas <abbasmomeny1994@gmail.com>
2024-05-29 17:34:32 +0330

    add: route for top transactions

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/transfer", [BankAccountController::class, 'transfer']);
 
+Route::get('top-users-transactions', [BankAccountController::class, 'topUsersWithTransactions']);
+
