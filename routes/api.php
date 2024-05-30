20adee10b718f7b424611ddeb62363491f1cebdc
abbas <abbasmomeny1994@gmail.com>
2024-05-30 07:38:30 +0330

    bind converter digit middleware to /transfer api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Controllers\BankAccountController;
+use App\Http\Middleware\NoneEngToEngDigit;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post("/transfer", [BankAccountController::class, 'transfer']);
+Route::post("/transfer", [BankAccountController::class, 'transfer'])->middleware(NoneEngToEngDigit::class);
 
 Route::get('top-users-transactions', [BankAccountController::class, 'topUsersWithTransactions']);
