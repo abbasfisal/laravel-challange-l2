91176a1c394ea2e8b5a6f50e1df22867d2397838
abbas <abbasmomeny1994@gmail.com>
2024-05-30 14:08:06 +0330

    add: TransactionResource

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -11,10 +12,31 @@ class TransactionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'source_card_id'      => $this->source_card_id,
-            'destination_card_id' => $this->destination_card_id,
-            'amount'              => $this->amount,
-            'created_at'          => $this->created_at
+            'id'     => $this['id'],
+            'type'   => $this->getTypeString(),
+            'amount' => $this['amount'],
         ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getTypeString(): string
+    {
+        $type = '';
+        switch ($this['type']) {
+            case Transaction::WITHDRAW:
+                $type = 'WITHDRAW';
+                break;
+
+            case Transaction::DEPOSIT:
+                $type = 'DEPOSIT';
+                break;
+
+            case Transaction::FEE:
+                $type = 'FEE';
+                break;
+        }
+        return $type;
     }
 }
